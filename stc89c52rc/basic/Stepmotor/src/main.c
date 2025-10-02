e4d697774e8da110bb90c0758219dcf83cd6707f
Unique9 <782638225@qq.com>
2025-10-02 21:15:32 +0800

    我不知道为什么speed加到3了后似乎就一直不变了, fuck

--- stc89c52rc/basic/Stepmotor/src/main.c
+++ stc89c52rc/basic/Stepmotor/src/main.c
@@ -1,30 +1,50 @@
-/**
- * CPU: 89C52
- * Freq: 12MHz
-*/
-
 #include <reg52.h>
-#include <stdio.h>
+#include "interrupt.h"
+#include "delay.h"
 
-void delay_100ms(void) //@12MHz
-{
-    unsigned char i, j;
+int count = 0, percent = 0;
 
-    i = 195;
-    j = 138;
-
-    do
-    {
-        while (--j)
-            ;
-    } while (--i);
-}
 
 void main(void)
 {
-    while (1)
-    {
-        P00 = !P00;
-        delay_100ms();
-    }
+	// 开启INT1中断
+	// intr_open_int1(1, 1);
+	// intr_open_int0(1, 1);
+	
+	// 设置定时器初值为65526, 即10us来一次中断
+	T0_open_method1(0, 0);
+	TH0 = 0xff;
+	TL0 = 0x9c;
+
+	int speed = 0;
+	while (1) {			
+		if (P31 == 0) {
+			while (P31 == 0);
+			speed++;
+			speed %= 4;
+			if (speed == 0) percent = 0;
+			else if (speed == 1) percent = 20;
+			else if (speed == 2) percent = 50;
+			else if (speed == 3) percent = 100;
+		}
+	}
 }
+
+void INT_T0_FUNC() interrupt 1
+{
+	// 定时器重新赋值
+	TH0 = 0xff;
+	TL0 = 0x9c;
+
+	count++;
+	count %= 100;
+	if (count < percent) {
+		P10 = 1;
+	}
+	else {
+		P10 = 0;
+	}
+
+}
+
+
